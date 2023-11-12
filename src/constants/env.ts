a834c7b74f387378f167eacb263346f9241510f6
datdang3007 <datdang3007@gmail.com>
2023-11-12 11:27:26 +0700

    feature/improve_api_method_get

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,11 +1,11 @@
 // ENV PRODUCT:
-// export enum PROCESS_ENV {
-//   BE_API_URL = "https://vivu-travel-be.adaptable.app/",
-//   DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1112290843236253767/qSvESemlahnPjVVBTu5HQPZbuOmmpBqFbo91zZS3K40-BW9GfpN4npmGkNXqaFznjMDs",
-// }
-
-// ENV DEVELOPMENT:
 export enum PROCESS_ENV {
-  BE_API_URL = "http://localhost:3000",
+  BE_API_URL = "https://vivu-travel-be.adaptable.app/",
   DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1112290843236253767/qSvESemlahnPjVVBTu5HQPZbuOmmpBqFbo91zZS3K40-BW9GfpN4npmGkNXqaFznjMDs",
 }
+
+// ENV DEVELOPMENT:
+// export enum PROCESS_ENV {
+//   BE_API_URL = "http://localhost:3000",
+//   DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1112290843236253767/qSvESemlahnPjVVBTu5HQPZbuOmmpBqFbo91zZS3K40-BW9GfpN4npmGkNXqaFznjMDs",
+// }
