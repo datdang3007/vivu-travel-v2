7f2f8162d68e12a3bb2fd58a5c8683917331f86e
datdang3007 <datdang3007@gmail.com>
2023-12-21 03:02:03 +0700

    feature/test_gg_map

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,13 +1,13 @@
 // ENV PRODUCT:
-// export enum PROCESS_ENV {
-//   BE_API_URL = "https://vivu-travel-be.adaptable.app/",
-//   GOOGLE_KEY = "AIzaSyBoJ1klc02Ot5AbdcIwxlqYbL6_Kql0LT0",
-//   DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1112290843236253767/qSvESemlahnPjVVBTu5HQPZbuOmmpBqFbo91zZS3K40-BW9GfpN4npmGkNXqaFznjMDs",
-// }
-
-// ENV DEVELOPMENT:
 export enum PROCESS_ENV {
-  BE_API_URL = "http://localhost:3000",
+  BE_API_URL = "https://vivu-travel-be.adaptable.app/",
   GOOGLE_KEY = "AIzaSyBoJ1klc02Ot5AbdcIwxlqYbL6_Kql0LT0",
   DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1112290843236253767/qSvESemlahnPjVVBTu5HQPZbuOmmpBqFbo91zZS3K40-BW9GfpN4npmGkNXqaFznjMDs",
 }
+
+// ENV DEVELOPMENT:
+// export enum PROCESS_ENV {
+//   BE_API_URL = "http://localhost:3000",
+//   GOOGLE_KEY = "AIzaSyBoJ1klc02Ot5AbdcIwxlqYbL6_Kql0LT0",
+//   DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1112290843236253767/qSvESemlahnPjVVBTu5HQPZbuOmmpBqFbo91zZS3K40-BW9GfpN4npmGkNXqaFznjMDs",
+// }
